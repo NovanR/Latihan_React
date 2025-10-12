2e870331edef0f2158e14f0f31a5f10b1ff51be9
Novan <novanramdan7@gmail.com>
2025-10-12 18:48:33 +0700

    Rendering List

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,18 +1,44 @@
 import CardProduct from "../components/Fragments/CardProduct";
 
+const Products = [
+  {
+    id: 1,
+    name: "Sepatu Baru",
+    price: "Rp 1.000.000",
+    image: "/images/shoes-1.jpg",
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores
+          necessitatibus, quas maxime cupiditate repudiandae quod quaerat quam
+          tempora veritatis doloribus labore ratione dicta magnam nobis porro
+          optio quisquam nulla architecto!`,
+  },
+  {
+    id: 2,
+    name: "Sepatu Lama",
+    price: "Rp 500.000",
+    image: "/images/shoes-1.jpg",
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit.`,
+  },
+  {
+    id: 3,
+    name: "Sepatu Neki",
+    price: "Rp 2.000.000",
+    image: "/images/shoes-1.jpg",
+    description: `Ini sepatu neki kaciw`,
+  },
+];
+
 const ProductsPage = () => {
   return (
     <div className="flex justify-center py-5">
-      <CardProduct>
-        <CardProduct.Header image="/images/shoes-1.jpg" />
-        <CardProduct.Body title="Sepatu Baru">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores
-          necessitatibus, quas maxime cupiditate repudiandae quod quaerat quam
-          tempora veritatis doloribus labore ratione dicta magnam nobis porro
-          optio quisquam nulla architecto!
-        </CardProduct.Body>
-        <CardProduct.Footer price="Rp 1.000.000" />
-      </CardProduct>
+      {Products.map((product) => (
+        <CardProduct>
+          <CardProduct.Header image={product.image} />
+          <CardProduct.Body name={product.name}>
+            {product.description}
+          </CardProduct.Body>
+          <CardProduct.Footer price={product.price} />
+        </CardProduct>
+      ))}
     </div>
   );
 };
